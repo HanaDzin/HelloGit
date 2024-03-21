a0d1660579c48d1dc6308d192ee036de7807cf5b
HanaDzin <dzinhana@hotmail.com>
2024-03-21 07:58:12 +0100

    added 2 lines

--- HelloGit/src/helloGitPackage/HelloGit.java
+++ HelloGit/src/helloGitPackage/HelloGit.java
@@ -3,6 +3,10 @@ package helloGitPackage;
 public class HelloGit {
 	public static void main (String[] args) {
 		System.out.println("Hello Git");
+		
+		System.out.println("Hello Git 2");
+		
+		System.out.println("Hello Git 3");
 	}
 
 }
